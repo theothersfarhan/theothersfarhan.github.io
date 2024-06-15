780e26c4c28f352abe7fb8fd7341617f1a920b70
nadir <nadirabbasforwork@gmail.com>
2024-06-15 10:07:14 +0500

    responsiveness

--- components/Container.vue
+++ components/Container.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mx-auto container">
+  <div class="mx-auto max-w-[1536px] relative">
     <slot />
   </div>
 </template>
--- components/Hero.vue
+++ components/Hero.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="w-full hero-gradient relative overflow-hidden">
-    <div class="bg-secondary container relative mx-auto p-14 min-h-[800px]">
-      <div class="absolute right-0 top-0 h-full bg-primary w-[450px]"></div>
+    <Container class="bg-secondary p-14 min-h-[100vh] xl:min-h-[700px]">
+      <div
+        class="absolute right-0 top-0 h-full bg-primary lg:w-[350px] xl:w-[450px]"
+      ></div>
       <Navbar
         :content="content"
         class="absolute top-0 z-10 w-full left-0 p-14"
       />
 
-      <div class="text-xl absolute top-1/2 -translate-y-1/2 ml-28">
-        <p class="text-primary font-medium mb-4">Hello Everyone.</p>
-        <h1 class="text-7xl font-bold text-white flex flex-col gap-3">
+      <div class="absolute top-1/2 -translate-y-1/2 ml-28">
+        <p class="text-primary font-medium mb-4 lg:text-sm xl:text-xl">
+          Hello Everyone.
+        </p>
+        <h1
+          class="lg:text-5xl xl:text-6xl font-bold text-white flex flex-col gap-3"
+        >
           <span>I'M FARHAN</span>
           <span>ABBAS</span>
         </h1>
-        <div class="bg-primary p-1 flex items-stretch max-w-max mt-14">
-          <p class="font-bold text-white bg-secondary px-8 py-2 max-w-[300px]">
+        <div
+          class="bg-primary p-1 flex items-stretch max-w-max lg:mt-8 xl:mt-14"
+        >
+          <p
+            class="flex items-center font-bold text-white bg-secondary lg:px-2 xl:px-4 py-2 lg:text-xs xl:text-base"
+          >
             {{ content.mainSection.designation }}
           </p>
-          <div class="p-5">
-            <ArrowRightIcon class="textsecondary w-8" />
+          <div class="lg:p-2 xl:p-3 flex items-center">
+            <ArrowRightIcon class="text-secondary lg:w-5 xl:w-6" />
           </div>
         </div>
       </div>
 
       <img
         :src="content.mainSection.heroImage.url"
-        class="absolute right-[280px] bottom-0 w-[400px]"
+        class="absolute lg:right-[220px] xl:right-[280px] bottom-0 lg:w-[300px] xl:w-[400px]"
       />
-    </div>
-    <DottedBox class="absolute bottom-0 left-0" />
+    </Container>
+    <DottedBox class="absolute bottom-0 left-0 -ml-3 -mb-3" />
   </div>
 </template>
 
